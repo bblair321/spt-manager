a2b4a574fadc012157887a4f828f1ff13227f53d
Brady Blair <bblair321@yahoo.comm>
2025-07-14 16:59:28 -0700

    automated build flow

--- forge.config.js
+++ forge.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
+      platforms: ["win32", "darwin"],
     },
     {
       name: "@electron-forge/maker-deb",
